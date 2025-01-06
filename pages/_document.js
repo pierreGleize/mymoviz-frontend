4478bc4ec4eaec13fa2013ace3b10525567bc6f5
Pierre Gleize <gleizepierre3gmail.com>
2025-01-06 19:33:48 +0100

    add icon to onglet

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="fr">
+        <Head>
+          <link rel="icon" href="/pop-corn.png" />
+          {/* <link rel="apple-touch-icon" href="/favicon.png" /> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
